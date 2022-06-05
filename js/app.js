4df5eb86d4b61d4071d020e604fc7fb2727abe0a
0xbwill <william.degh@gmail.com>
2022-06-05 15:56:25 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// NavSlide function to translate navbar when burgerAnimation is toggled
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.mobile-menu');
+    const closeBtn = document.querySelector('.close-btn');
+
+    // Event listener on nav-active
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('change');
+        nav.classList.toggle('active');
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    });
+
+    closeBtn.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        burger.classList.toggle('change');
+        document.body.style.backgroundColor = "rgba(0,0,0,0)";
+    });
+}
+
+navSlide();
