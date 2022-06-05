93294f819711d1db042db2856368570000fe0b9a
0xbwill <william.degh@gmail.com>
2022-06-05 16:05:04 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 // NavSlide function to translate navbar when burgerAnimation is toggled
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.mobile-menu');
-    const closeBtn = document.querySelector('.close-btn');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".mobile-menu");
+  const closeBtn = document.querySelector(".close-btn");
 
-    // Event listener on nav-active
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('change');
-        nav.classList.toggle('active');
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    });
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("change");
+    nav.classList.toggle("active");
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  });
 
-    closeBtn.addEventListener('click', () => {
-        nav.classList.toggle('active');
-        burger.classList.toggle('change');
-        document.body.style.backgroundColor = "rgba(0,0,0,0)";
-    });
-}
+  closeBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("change");
+    document.body.style.backgroundColor = "hsl(0, 0%, 98%)";
+  });
+};
 
 navSlide();
